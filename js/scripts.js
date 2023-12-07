3b09ce20273ab75aa6252f07b814e2d096fec4b2
DiegoFarrugio <diegofarrugio0@gmail.com>
2023-12-07 15:45:57 +0100

    Creazione repo e template js/html/css

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+//MILESTONE 1
+// - Creiamo il markup statico
+// - Costruiamo il container e inseriamo un'immagine grande al centro
+// - Avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
+
+
